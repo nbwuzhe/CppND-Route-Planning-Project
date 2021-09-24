f7aa223615d5c7160fdf28519278ff61e9d84da3
Tim Wu <tim.wuzhe@gmail.com>
2021-09-24 19:51:21 -0400

    Modification for main.cpp for Astar Searching.

--- src/main.cpp
+++ src/main.cpp
@@ -55,6 +55,16 @@ int main(int argc, const char **argv)
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
+    float start_x, start_y, end_x, end_y;
+    std::cout << "Please input the coordinate of start and end point (startX startY endX endY)" << std::endl;
+    std::cout << "X coordinate for start point: ";
+    std::cin >> start_x;
+    std::cout << "Y coordinate for start point: ";
+    std::cin >> start_y;
+    std::cout << "X coordinate for end point: ";
+    std::cin >> end_x;
+    std::cout << "Y coordinate for end point: ";
+    std::cin >> end_y;
 
     // Build Model.
     RouteModel model{osm_data};
